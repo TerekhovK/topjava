7c16c6dd0b02faedafa2f89b0ace53bf33e99986
TerekhovK <myyyk12@gmail.com>
2017-07-18 15:51:46 +0300

    Update MealDAO.java

--- src/main/java/ru/javawebinar/topjava/dao/MealDAO.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDAO.java
@@ -1,17 +1,10 @@
 package ru.javawebinar.topjava.dao;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import ru.javawebinar.topjava.model.Meal;
 
-
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 
 public interface MealDAO {
