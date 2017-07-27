05a4504fd965d46296297cfe8f0c88630fd6cf15
TerekhovK <myyyk12@gmail.com>
2017-07-27 23:18:00 +0300

    HW03 base part

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,6 +4,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.jdbc.JdbcUserRepositoryImpl;
 import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -20,15 +21,15 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userNa33me", "email22@emal", "password", Role.ROLE_ADMIN));
             System.out.println();
-
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
+            adminUserController.getAll().forEach(System.out::println);
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealWithExceed> filteredMealsWithExceeded =
+//                    mealController.getBetween(
+//                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExceeded.forEach(System.out::println);
         }
     }
 }
