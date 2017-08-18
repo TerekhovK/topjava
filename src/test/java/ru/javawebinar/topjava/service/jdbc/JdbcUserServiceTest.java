0c98d6e8ab3ac0e3a0fd6cc6dc2b47c0dc2a72c6
TerekhovK <myyyk12@gmail.com>
2017-08-18 13:46:00 +0300

    HW06 jdbc tests fix

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,14 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
 }
